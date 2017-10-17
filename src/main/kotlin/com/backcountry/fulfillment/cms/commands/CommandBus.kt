ba13deddf3879b9ab98fe83463e3204ff7ee222b
Edwin Dalorzo <edalorzo@backcountry.com>
2017-10-17 00:19:16 -0600

    Initial remote command definition

--- src/main/kotlin/com/backcountry/fulfillment/cms/commands/CommandBus.kt
+++ src/main/kotlin/com/backcountry/fulfillment/cms/commands/CommandBus.kt
@@ -1,6 +1,7 @@
 package com.backcountry.fulfillment.cms.commands
 
 import com.backcountry.fulfillment.cqrs.commands.api.Command
+import com.backcountry.fulfillment.cqrs.commands.api.RemoteCommand
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
@@ -12,6 +13,10 @@ class CommandBus @Autowired constructor(private val publisher: ApplicationEventP
     fun publishCommand(command: Command) {
         Objects.requireNonNull(command, "The command must not be null")
         publisher.publishEvent(command)
+    }
+
+    fun publishCommand(commnd: RemoteCommand) {
+        //TODO send the remote command
     }
 
 }
